7ba11603e852cef1f0515bfa1af4f25600b1c59f
sergeyzelentsov <_sharapov_@mail.ru>
2023-03-21 13:42:26 +0300

    hw06

---
+++ hw06/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int readConsole(string msg){
+    Console.WriteLine(msg);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int a = readConsole("Задача 42 (необязательно): Напишите программу, которая будет преобразовывать десятичное число в двоичное. \r\n Введите число");
+String result = "";
+while(a!=0){
+    result = result + a % 2;
+    a = a/2;
+}
+char[] arr = result.ToCharArray();
+Array.Reverse(arr);
+Console.WriteLine(new string(arr));
+
